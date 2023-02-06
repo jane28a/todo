5a7ff2e32af23c2735444d909052c1965841a97d
Yevheniia Mishchishyna <janekaraim@gmail.com>
2023-02-06 21:19:47 +0200

    details view added

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from tasks.models import Task
 
@@ -8,3 +9,10 @@ def index(request):
         'tasks': Task.objects.all()
     }
     return render(request, 'tasks/tasks.html', context=context)
+
+def details(request, task_id):
+    try:
+        task = Task.objects.get(id=task_id)
+    except Task.DoesNotExist:
+        raise Http404('Task does not exist')
+    return render(request, 'tasks/details.html', context={'task': task})
