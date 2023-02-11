3088292e0609f4583d8db0fd8613ba36d5edb8c4
Yevheniia Mishchishyna <janekaraim@gmail.com>
2023-02-11 14:05:41 +0200

    lesson 1102

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.contrib.auth.decorators import login_required
 
 from tasks.models import Task
 
-
+@login_required
 def index(request):
+    user = request.user
     context = {
-        'tasks': Task.objects.all()
+        'tasks': user.task_set.all()
     }
     return render(request, 'tasks/list.html', context=context)
 
